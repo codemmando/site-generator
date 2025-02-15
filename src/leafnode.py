33ae0275366bf107c4ba6fa7bddf6535f6960dc0
codemmando <rene.mikkelsen@protonmail.com>
2025-02-15 08:45:32 +0100

    adding leaf node

---
+++ src/leafnode.py
@@ -0,0 +1,9 @@
+import htmlnode as HTMLNode
+
+class LeafNode(HTMLNode):
+    
+    def __init__(self, tag, value, props=None):
+        super(self,tag, value, props).__init__()
+
+    def to_html():
+        
