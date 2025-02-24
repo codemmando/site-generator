bef5791963b86c61e7dc5e15e834206f0f7316ac
codemmando <rene.mikkelsen@protonmail.com>
2025-02-24 07:56:38 +0100

    last of project

--- src/leafnode.py
+++ src/leafnode.py
@@ -6,12 +6,11 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
-            raise ValueError("LeafNode: value cannot be None.")
-        if not self.tag:
-            return self.value    
-        else:
-            return f"<{self.tag}{self.__get_props()}>{self.value}</{self.tag}>"
-        
-    def __get_props(self):
-        return self.props_to_html()    
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
